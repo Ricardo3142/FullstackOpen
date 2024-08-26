39937e98aaeb1692aee834bd5a79e192dccc1ecc
Ricardo3142 <davidricardoarias@hotmail.com>
2024-08-26 16:48:27 -0700

    test: c) component state, event handlers

---
+++ test/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+//Component Display
+const Display = ({counter}) => <div>{counter}</div>
+
+
+//Component Button
+const Button = ({onClick, text}) => <button onClick={onClick}> {text} </button>
+
+
+const App = () => {
+  const [counter, setCounter] = useState(0)
+  console.log('rendering with counter value', counter)
+
+  const increaseByOne = () => {
+    console.log('increasing, value before', counter)
+    setCounter(counter + 1)
+  }
+
+  const decreaseByOne = () => { 
+    console.log('decreasing, value before', counter)
+    setCounter(counter - 1)
+  }
+
+  const setToZero = () => {
+    console.log('resetting to zero, value before', counter)
+    setCounter(0)
+  }
+
+  return (
+    <div>
+      <Display counter={counter} />
+      <Button onClick={increaseByOne} text="plus" />
+      <Button onClick={setToZero} text="zero" />
+      <Button onClick={decreaseByOne} text="minus" />
+    </div>
+  )
+} 
+
+export default App
